33c2e74a16d4968cd61a61ff7ded517781666af2
Gufran Ansari <61289648+gufran-ansari@users.noreply.github.com>
2024-01-19 19:24:45 +0530

    Add files via upload
    Bug Resolved

--- app.js
+++ app.js
@@ -1,57 +1,57 @@
- function togglePlan() {
- 	var checkbox = document.getElementById( 'planCheckbox' );
- 	var monthlyTable = document.getElementById( 'monthlyTable' );
- 	var yearlyTable = document.getElementById( 'yearlyTable' );
+function togglePlan() {
+	var checkbox = document.getElementById( 'planCheckbox' );
+	var monthlyTable = document.getElementById( 'monthlyTable' );
+	var yearlyTable = document.getElementById( 'yearlyTable' );
 
- 	if ( checkbox.checked ) {
- 		monthlyTable.style.display = 'none';
- 		yearlyTable.style.display = 'table';
- 		document.getElementById( 'planCheckbox' ).classList.add( 'active' );
- 	} else {
- 		monthlyTable.style.display = 'table';
- 		yearlyTable.style.display = 'none';
- 		document.getElementById( 'planCheckbox' ).classList.remove( 'active' );
- 	}
- }
-
-
- //  function openTab( evt, cityName ) {
- //  	document.getElementById( "defaultOpen" ).click();
-
- //  	// Declare all variables
- //  	var i, tabcontent, tablinks;
-
- //  	// Get all elements with class="tabcontent" and hide them
- //  	tabcontent = document.getElementsByClassName( "tabcontent" );
- //  	for ( i = 0; i < tabcontent.length; i++ ) {
- //  		tabcontent[ i ].style.display = "none";
- //  	}
-
- //  	// Get all elements with class="tablinks" and remove the class "active"
- //  	tablinks = document.getElementsByClassName( "tablinks" );
- //  	for ( i = 0; i < tablinks.length; i++ ) {
- //  		tablinks[ i ].className = tablinks[ i ].className.replace( " active", "" );
- //  	}
-
- //  	// Show the current tab, and add an "active" class to the button that opened the tab
- //  	document.getElementById( cityName ).style.display = "block";
- //  	evt.currentTarget.className += " active";
- //  }
+	if ( checkbox.checked ) {
+		monthlyTable.style.display = 'none';
+		yearlyTable.style.display = 'table';
+		document.getElementById( 'planCheckbox' ).classList.add( 'active' );
+	} else {
+		monthlyTable.style.display = 'table';
+		yearlyTable.style.display = 'none';
+		document.getElementById( 'planCheckbox' ).classList.remove( 'active' );
+	}
+}
 
 
+//  function openTab( evt, cityName ) {
+//  	document.getElementById( "defaultOpen" ).click();
 
- document.getElementById( "defaultOpen" ).click();
+//  	// Declare all variables
+//  	var i, tabcontent, tablinks;
 
- function openTab( evt, cityName ) {
- 	var i, tabcontent, tablinks;
- 	tabcontent = document.getElementsByClassName( "tabcontent" );
- 	for ( i = 0; i < tabcontent.length; i++ ) {
- 		tabcontent[ i ].style.display = "none";
- 	}
- 	tablinks = document.getElementsByClassName( "tablinks" );
- 	for ( i = 0; i < tablinks.length; i++ ) {
- 		tablinks[ i ].className = tablinks[ i ].className.replace( " active", "" );
- 	}
- 	document.getElementById( cityName ).style.display = "block";
- 	evt.currentTarget.className += " active";
- }
+//  	// Get all elements with class="tabcontent" and hide them
+//  	tabcontent = document.getElementsByClassName( "tabcontent" );
+//  	for ( i = 0; i < tabcontent.length; i++ ) {
+//  		tabcontent[ i ].style.display = "none";
+//  	}
+
+//  	// Get all elements with class="tablinks" and remove the class "active"
+//  	tablinks = document.getElementsByClassName( "tablinks" );
+//  	for ( i = 0; i < tablinks.length; i++ ) {
+//  		tablinks[ i ].className = tablinks[ i ].className.replace( " active", "" );
+//  	}
+
+//  	// Show the current tab, and add an "active" class to the button that opened the tab
+//  	document.getElementById( cityName ).style.display = "block";
+//  	evt.currentTarget.className += " active";
+//  }
+
+
+
+document.getElementById( "defaultOpen" ).click();
+
+function openTab( evt, cityName ) {
+	var i, tabcontent, tablinks;
+	tabcontent = document.getElementsByClassName( "tabcontent" );
+	for ( i = 0; i < tabcontent.length; i++ ) {
+		tabcontent[ i ].style.display = "none";
+	}
+	tablinks = document.getElementsByClassName( "tablinks" );
+	for ( i = 0; i < tablinks.length; i++ ) {
+		tablinks[ i ].className = tablinks[ i ].className.replace( " active", "" );
+	}
+	document.getElementById( cityName ).style.display = "block";
+	evt.currentTarget.className += " active";
+}
